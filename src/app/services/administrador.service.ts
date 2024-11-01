36354b4e857c69d0ec7e8ce730c3e56952c08cdd
marceloquirogasaavedra <12h09icsos@gmail.com>
2024-11-01 11:16:37 -0400

    m

--- src/app/services/administrador.service.ts
+++ src/app/services/administrador.service.ts
@@ -6,18 +6,17 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AdministradorService {
-  private url = 'http://192.168.60.3:8080/'
-  private url2='http://localhost:8080/'
+  private url='http://localhost:8080/'
 
 
   constructor(private http: HttpClient) {}
   obtenertoken(): Observable<any>{
-    return this.http.get(this.url+'/login')
+    return this.http.get(this.url+'login')
   }
   login(credentials: { correo: string, password: string }): Observable<any> {
     return this.http.post(this.url  +  'login', credentials);
   }
   crearMedico(credentials:{nombre:string,apellido_paterno:string,apellido_materno:string,estado:boolean,email:string,id_sucursal_especialidad:BigInt}):Observable<any>{
-    return this.http.post(this.url2 + 'medico/crear',credentials)
+    return this.http.post(this.url + 'medico/crear',credentials)
   }
 }
